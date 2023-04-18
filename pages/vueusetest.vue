9d98d2b0572c670651763ba2779c6974030a83ee
marcpaget <marc.d.paget@gmail.com>
2023-04-19 01:47:15 +0200

    feat: improved vueusetest page

--- pages/vueusetest.vue
+++ pages/vueusetest.vue
@@ -5,11 +5,29 @@ const options = {
     maximumAge: 0,
 }
 const { coords, locatedAt, error, resume, pause } = useGeolocation(options)
+let speedkmh = computed(() => {
+    if (coords.value.speed) {
+        return (coords.value.speed * 3.6).toFixed(2)
+    }
+    return 0
+})
+let altitude = computed(() => {
+    if (coords.value.altitude) {
+        return coords.value.altitude.toFixed(2)
+    }
+    return 0
+})
+let accurateAltitude = computed(() => {
+    if (coords.value.altitude) {
+        return coords.value.altitude.toFixed(2)
+    }
+    return 0
+})
 </script>
 
 <template>
     <div>
-        <pre lang="json">{{
+        <!-- <pre lang="json">{{
             JSON.stringify(
                 {
                     coords: {
@@ -27,8 +45,31 @@ const { coords, locatedAt, error, resume, pause } = useGeolocation(options)
                 null,
                 2
             )
-        }}</pre>
-        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="pause">Pause watch</button>
-        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="resume">Start watch</button>
+        }}</pre> -->
+        <p>Speed: {{ speedkmh }} km/h</p>
+        <div class="stats bg-primary text-primary-content">
+            <div class="stat">
+                <div class="stat-title">Speed</div>
+                <div class="stat-value">{{ speedkmh }}</div>
+                <div class="stat-desc">KM/T</div>
+                <div class="stat-actions">
+                    <button class="btn btn-sm btn-success" @click="resume">Start watch</button>
+                </div>
+            </div>
+
+            <div class="stat">
+                <div class="stat-title">Approx. altitude</div>
+                <div class="stat-value">{{ altitude }}</div>
+                <div class="stat-desc">M.O.H.</div>
+            </div>
+            <div class="stat">
+                <div class="stat-title">Accurate altitude</div>
+                <div class="stat-value">{{ accurateAltitude }}</div>
+                <div class="stat-desc">m.o.h.</div>
+                <div class="stat-actions">
+                    <button class="btn btn-sm" @click="pause">Pause watch</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
